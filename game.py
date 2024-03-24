888f96ea53eb3cb7b3f1ea8104275d7f6916daff
Gowtham04krishna <gowtham04krishna@gmail.com>
2024-03-24 22:14:53 +0530

    struct
    struct

--- game.py
+++ game.py
@@ -1,18 +1,3 @@
-from tkinter import *
-import random
-
-
-root =Tk()
-root.geometry("400x450")
-root.title("tic tac toe")
-
-frame1=Frame(root)
-frame1.pack()
-titleLabel=Label(frame1,text="Tic Tac Toe",font=("arial",30),)
-titleLabel.pack()
-
-frame2=Frame(root)
-frame2.pack()
 
 class tictac():
     def __init__(self):
@@ -20,37 +5,13 @@ class tictac():
         self.computer = []
         self.empty = [1,2,3,4,5,6,7,8,9]
         self.cells = [" "," "," "," "," "," "," "," "," "]
-        self.display()
+
     def display(self):
-
-        box0=Button(frame2,text=self.cells[0],width=12,height=6)
-        box0.grid(row=0,column=0)
-
-        box1=Button(frame2,text=self.cells[1],width=12,height=6)
-        box1.grid(row=0,column=1)
-
-        box2=Button(frame2,text=self.cells[2],width=12,height=6)
-        box2.grid(row=0,column=2)
-
-
-        box3=Button(frame2,text=self.cells[3],width=12,height=6)
-        box3.grid(row=1,column=0)
-
-        box4=Button(frame2,text=self.cells[4],width=12,height=6)
-        box4.grid(row=1,column=1)
-
-        box5=Button(frame2,text=self.cells[5],width=12,height=6)
-        box5.grid(row=1,column=2)
-
-
-        box6=Button(frame2,text=self.cells[6],width=12,height=6)
-        box6.grid(row=2,column=0)
-
-        box7=Button(frame2,text=self.cells[7],width=12,height=6)
-        box7.grid(row=2,column=1)
-
-        box8=Button(frame2,text=self.cells[8],width=12,height=6)
-        box8.grid(row=2,column=2)
+        print(f" {self.cells[0]} | {self.cells[1]} | {self.cells[2]} ")
+        print("---|---|---")
+        print(f" {self.cells[3]} | {self.cells[4]} | {self.cells[5]} ")
+        print("---|---|---")
+        print(f" {self.cells[6]} | {self.cells[7]} | {self.cells[8]} ")
 
     def computerInsert(self):
         y = 0
@@ -266,5 +227,3 @@ class tictac():
 
 
 Tc= tictac()
-Tc.display()
-root.mainloop()
